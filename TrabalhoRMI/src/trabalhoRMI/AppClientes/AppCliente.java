8135f6e9e7ffe831a5ff803b97749336bc9b4c04
akrafzik <alec.k.haroutiounian@hotmail.com>
2017-05-05 00:03:37 -0300

    added client app basic structure

---
+++ TrabalhoRMI/src/trabalhoRMI/AppClientes/AppCliente.java
@@ -0,0 +1,14 @@
+package trabalhoRMI.AppClientes;
+
+import java.rmi.*;
+import java.util.Scanner;
+
+public class AppCliente {
+    public static void main(String[] args) {
+        try{
+            
+        } catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
